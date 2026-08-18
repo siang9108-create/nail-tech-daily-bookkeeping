34159c87c8eab3bb91094122d4fd84362f30175b
siang9108-create <siang9108@gmail.com>
2026-08-18 07:41:05 -0700

    Add service worker for nail tech PWA

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='soulcool-nail-tech-v1';
+const APP=['./','./index.html','./manifest.webmanifest','./app-icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
